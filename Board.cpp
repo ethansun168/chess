d74414ad7902787c452f135411e89c63e5f26654
Fazanza <wynnmka@gmail.com>
2023-02-16 10:54:29 -0500

    Added fencodeToBoard changability

--- Board.cpp
+++ Board.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 Board::Board() {
+}
 
+void Board::fenCodeToBoard(string fenCode, ostream& os) {
+	int rowNumber = 0, charNumber = 0, stringIndex = 0;
+	while (fenCode[stringIndex] != ' ') {
+		int numberOfBlanks = fenCode[stringIndex] - '0';
+		if (fenCode[stringIndex] == '/') {
+			os << "\n";
+			charNumber = 0;
+			rowNumber++;
+		}
+		else if (numberOfBlanks > 0 && numberOfBlanks < 9) {
+			for (int i = 0; i < numberOfBlanks; ++i) {
+				os << ". ";
+			}
+			charNumber += numberOfBlanks;
+		} else {
+			os << fenCode[stringIndex] << " ";
+			charNumber++;
+		}
+		stringIndex++;
+	}
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "Board.h";
 
 using namespace std;
 
 int main() {
+	Board board;
 	return 0;
 }
