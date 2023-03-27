ee496b771ab9f89d409bc50ccb125482358c22eb
ethansun <ethansun168@gmail.com>
2023-03-26 23:05:24 -0400

    Added game loop and input parsing

--- main.cpp
+++ main.cpp
@@ -33,24 +33,34 @@ ostream& operator << (ostream& os, Color color) {
 }
 
 int main() {	
-	Board board("r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1");
-	cout << "Starting board" << endl;
-	cout << board << endl;
-	cout << board.getColor() << "'s turn" << endl;
-	cout << board.move(convert('e', 1), convert('g', 1));
-	cout << board << endl;
-	//board.move({ 7,4 }, { 7,6 });
-	cout << board.getColor() << "'s turn" << endl;
-	cout << board.move({ 0,1 }, { 2,2 });
-	cout << board;
-	cout << endl;
-	Board board2;
-	cout << "Starting board\n" << board2;
-	board2.move(convert('e', 2), convert('e', 4));
-	cout << "Pawn to e4\n" << board2;
-	board2.undo();
-	cout << "Undo\n" << board2;
+	Board board;
+	cout << board.getColor() << "'s turn\n";
+	while (!board.isCheckMate()) {
+		cout << board;
+		cout << "Enter your move: first two is start, last two is end\n[FileRank FileRank]\n";
+		char startFile;
+		char endFile;
+		int startRank;
+		int endRank;
+		cin >> startFile >> startRank >> endFile >> endRank;
+		while (startFile < 'a' || 
+			startFile > 'h' || 
+			startRank < 1 || 
+			startRank > 8 ||
+			endFile < 'a' || 
+			endFile > 'h' || 
+			endRank < 1 || 
+			endRank > 8) {
+			cout << "Invalid input! Enter something valid:\n";
+			cin.clear();
+			string temp;
+			getline(cin, temp);
+			
+			cin >> startFile >> startRank >> endFile >> endRank;
+		}
+		cout << board.move(convert(startFile, startRank), convert(endFile, endRank)) << endl;
+		cout << board.getColor() << "'s turn\n";
 
-	cout << board2.getPiece(convert('a', 3)).isEmpty();
+	}
 	return 0;
 }
